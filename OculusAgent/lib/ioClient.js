a7390caae1532018bb75a15b5561b748a734eb7e
Dylan <vuzdylan@gmail.com>
2015-04-05 13:22:40 -0400

    ioClient added

---
+++ OculusAgent/lib/ioClient.js
@@ -0,0 +1,25 @@
+var socket = io.connect('http://129.21.208.59:8125',{secure:false});
+function openConnection(){
+  socket.on('connect', function () {
+    console.log('client connected');
+    socket.emit('init','{"from" : "Oculus"}');
+    //var obj = { size : 20, name: "Jason" };
+    //var data = JSON.stringify(obj);
+    //socket.emit('data', data);
+  });
+
+  socket.on('disconnect', function(){
+    console.log("opps client disconnect");
+  });
+
+  socket.on('move',function(data){
+    console.log("moving torso");
+    controls.move(data.move);
+  });
+
+  socket.on('draw',function(data){
+    var start = new THREE.Vector3(data.start.x,data.start.y,data.start.z);
+    var end = new THREE.Vector3(data.stop.x,data.stop.y,data.z);
+    drawLine(start,end);
+  });
+}
